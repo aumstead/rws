c943c132c6afde0a5552b2d4ca9ad8271a7e6325
unknown <andrew.umstead@gmail.com>
2021-10-14 14:57:41 +0800

    edits after vid meeting

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Hero text animation JS
-const spanEl = document.getElementById("hero-anim");
-let count = 0;
-heroTextAnimation();
+// const spanEl = document.getElementById("hero-anim");
+// let count = 0;
+// heroTextAnimation();
 
-const texts = ["Lorem Warranty", "Lorem Safety", "Lorem Coverage"];
+// const texts = ["Lorem Warranty", "Lorem Safety", "Lorem Coverage"];
 
-function heroTextAnimation() {
-  setInterval(() => {
-    if (count > 2) count = 0;
-    spanEl.classList.remove("animate-hero-text");
-    spanEl.innerText = texts[count];
-    count++;
-    setTimeout(() => {
-      spanEl.classList.add("animate-hero-text");
-    }, 100);
-  }, 5200);
-}
+// function heroTextAnimation() {
+//   setInterval(() => {
+//     if (count > 2) count = 0;
+//     spanEl.classList.remove("animate-hero-text");
+//     spanEl.innerText = texts[count];
+//     count++;
+//     setTimeout(() => {
+//       spanEl.classList.add("animate-hero-text");
+//     }, 100);
+//   }, 5200);
+// }
